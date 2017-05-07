06a104a962420626dd7bc70cb2b795f69b583740
Do Gia Dat <dogiadat@gmail.com>
2017-05-07 12:48:49 +0700

    fix after login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth request.env["omniauth.auth"]
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user
+      redirect_to root_path
       set_flash_message :notice, :success, kind: "Facebook" if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
@@ -15,7 +16,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth request.env["omniauth.auth"]
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user
+      redirect_to root_path
       set_flash_message :notice, :success, kind: "Google" if is_navigational_format?
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
