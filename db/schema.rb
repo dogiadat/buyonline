973fd35d015819c5bd17a5079f63bb09984a4235
Do Gia Dat <dogiadat@gmail.com>
2017-05-07 10:22:40 +0700

    fix env

--- db/schema.rb
+++ db/schema.rb
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 20170419065004) do
     t.float    "long"
     t.text     "description"
     t.integer  "min_order"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
   create_table "slideshows", force: :cascade do |t|
@@ -178,8 +178,8 @@ ActiveRecord::Schema.define(version: 20170419065004) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
